8642001797ed68fa128dccd211511d769a70c58e
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-03-21 21:41:13 +0530

    Adding required imports to product.service.ts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+
 
 @Injectable({
   providedIn: 'root'
