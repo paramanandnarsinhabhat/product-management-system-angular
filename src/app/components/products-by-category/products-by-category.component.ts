b5866dd5a1555ac563f2ef7c6f167a11ec80391f
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-03-22 12:41:26 +0530

    changes made to component and html for products  by category

--- src/app/components/products-by-category/products-by-category.component.ts
+++ src/app/components/products-by-category/products-by-category.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
+
+
 
 @Component({
   selector: 'app-products-by-category',
-  standalone: true,
-  imports: [],
   templateUrl: './products-by-category.component.html',
   styleUrl: './products-by-category.component.scss'
 })
-export class ProductsByCategoryComponent {
 
+export class ProductsByCategoryComponent implements OnInit {
+  products: Product[] = [];
+  category: string = '';
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
+
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.category = params['category'];
+      if (this.category) {
+        this.loadProductsByCategory(this.category);
+      }
+    });
+  }
+
+  loadProductsByCategory(category: string): void {
+    this.productService.getProductsByCategory(category).subscribe(
+      (products: Product[]) => {
+        this.products = products;
+      },
+      error => {
+        console.error(`Error occurred while fetching products for category ${category}`, error);
+      
+      }
+    );
+  }
+  
 }
