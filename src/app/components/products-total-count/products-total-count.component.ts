a70310949012132eeb3a959c74f758b1a0a3ce71
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-03-22 12:24:26 +0530

    products total count component

--- src/app/components/products-total-count/products-total-count.component.ts
+++ src/app/components/products-total-count/products-total-count.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+
 
 @Component({
   selector: 'app-products-total-count',
-  standalone: true,
   imports: [],
   templateUrl: './products-total-count.component.html',
   styleUrl: './products-total-count.component.scss'
 })
-export class ProductsTotalCountComponent {
+export class ProductsTotalCountComponent implements OnInit  {
 
+  totalCount: number = 0;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getTotalCount().subscribe({
+      next: (count: number) => {
+        this.totalCount = count;
+      },
+      error: (error) => {
+        console.error('Error fetching total count of products', error);
+        // Implement your error handling logic here
+      },
+      complete: () => console.log('Fetch total count completed') // Optional
+    });
+  }
+  
 }
