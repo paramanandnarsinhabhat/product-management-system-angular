7564f98434212d78eadecb38a3cc4c0be8fab17f
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-03-22 15:24:11 +0530

    Adding all the required components in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,30 @@ import { ProductCreateComponent } from './components/product-create/product-crea
 import { ProductEditComponent } from './components/product-edit/product-edit.component';
 // Import your services
 import { ProductService } from './services/product.service';
+import { AllProductsDeleteComponent } from './components/all-products-delete/all-products-delete.component';
+import { ProductSearchComponent } from './components/product-search/product-search.component';
+import { ProductsSortComponent } from './components/product-sort/product-sort.component';
+import { ProductsByCategoryComponent } from './components/products-by-category/products-by-category.component';
+import { ProductsTotalCountComponent } from './components/products-total-count/products-total-count.component';
+import { SingleProductDeleteComponent } from './components/single-product-delete/single-product-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     // Declare your components here
-    ProductsListComponent,
-    ProductDetailsComponent,
+    AllProductsDeleteComponent,
     ProductCreateComponent,
-    ProductEditComponent
+    ProductDetailsComponent,
+    ProductEditComponent,
+    ProductSearchComponent,
+    ProductsSortComponent,
+    ProductsByCategoryComponent,
+    ProductsListComponent,
+    ProductsTotalCountComponent,
+    SingleProductDeleteComponent
+    
+
+  
   ],
   imports: [
     BrowserModule,
