de98e42f67dd564845f8a6e8a937acd5be215589
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-03-22 14:09:18 +0530

    new component product sort created.

---
+++ src/app/components/product-sort/product-sort.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-sort',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-sort.component.html',
+  styleUrl: './product-sort.component.scss'
+})
+export class ProductSortComponent {
+
+}
