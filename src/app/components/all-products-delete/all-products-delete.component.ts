cc6915563cf375d9705e40216f0f40551cc6628d
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-03-22 14:57:50 +0530

    all-products-delete component has deleteAllProducts method implemented

--- src/app/components/all-products-delete/all-products-delete.component.ts
+++ src/app/components/all-products-delete/all-products-delete.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
+
 
 @Component({
   selector: 'app-all-products-delete',
-  standalone: true,
-  imports: [],
   templateUrl: './all-products-delete.component.html',
   styleUrl: './all-products-delete.component.scss'
 })
-export class AllProductsDeleteComponent {
 
+export class AllProductsDeleteComponent {
+  constructor(private productService: ProductService) {}
+
+  deleteAllProducts(): void {
+    this.productService.deleteAllProducts().subscribe({
+      next: () => {
+        console.log('All products deleted successfully');
+        // Handle successful deletion, such as refreshing the list or showing a message
+      },
+      error: (error) => {
+        console.error('Error deleting all products', error);
+        // Handle errors, such as showing an error message to the user
+      }
+    });
+  }
 }
