3c4b36108775c3d219bc7bce9eae37d3818ed674
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-03-22 11:41:21 +0530

    Function to search products by name

--- src/app/components/product-search/product-search.component.ts
+++ src/app/components/product-search/product-search.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { Product } from '../../models/product';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-search',
-  standalone: true,
   imports: [],
   templateUrl: './product-search.component.html',
   styleUrl: './product-search.component.scss'
 })
 export class ProductSearchComponent {
+  products: Product[] = [];
+  searchQuery: string = '';
 
+  constructor(private productService: ProductService) {}
+
+  searchProducts(): void {
+    if (this.searchQuery) {
+      this.productService.searchProductsByName(this.searchQuery).subscribe(
+        (data: Product[]) => {
+          this.products = data;
+        },
+        error => {
+          // Handle error here, e.g., showing an error message
+          console.error('Error occurred while searching for products', error);
+        }
+      );
+    }
+
+}
 }
