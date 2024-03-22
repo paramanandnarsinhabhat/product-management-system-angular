f23cb8ada93b5d922c8051ec0e01db3192fa1d6d
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-03-22 12:33:00 +0530

    products by category component generated

---
+++ src/app/components/products-by-category/products-by-category.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-by-category',
+  standalone: true,
+  imports: [],
+  templateUrl: './products-by-category.component.html',
+  styleUrl: './products-by-category.component.scss'
+})
+export class ProductsByCategoryComponent {
+
+}
